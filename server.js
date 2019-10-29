86822127ac925af237157be122962987b7f660e6
Vick Greenfields <akintoluvic@users.noreply.github.com>
2019-10-29 15:03:19 +0100

    init

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const http = require('http');
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
 
-const server = http.createServer((req, res) => {
-    res.send('This first response');
-})
+app.get('/', (req, res) => res.send('Hello World!'))
 
-server.listen(process.env.PORT || 3000)
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
