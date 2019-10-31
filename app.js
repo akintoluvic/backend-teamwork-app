d890c5ca7cdb3fccd12862669f224de884f44d60
Vick Greenfields <akintoluvic@users.noreply.github.com>
2019-10-31 19:43:02 +0100

    cors fix

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 const express = require('express')
+const bodyParser = require('body-parser');
 const { Client } = require('pg')
 const app = express()
 const client = new Client()
 
 const connectionString = "postgres://postgres:greene@localhost::1/greene-one";
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+
+app.use(bodyParser.json());
 
 app.get('/', (req, res, next) => {
     res.json({
