759cb177e09ea7346c046ebd5688ef78500c345a
Vick Greenfields <akintoluvic@users.noreply.github.com>
2019-10-31 20:26:42 +0100

    fix

--- server.js
+++ server.js
@@ -8,10 +8,4 @@ app.set('port', port);
 
 const server = http.createServer(app)
 
-// server.listen(port)
-
 if(!module.parent){ server.listen(port); }
-
-// exports.closeServer = function(){
-//     server.close();
-//   };
