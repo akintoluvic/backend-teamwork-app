59cedcf7e5c2f743898c377ff30e4fa990cc127f
Vick Greenfields <akintoluvic@users.noreply.github.com>
2019-10-31 18:46:32 +0100

    fix

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const express = require('express')
+const { Client } = require('pg')
 const app = express()
+const client = new Client()
+
+const connectionString = "postgres://postgres:greene@localhost::1/greene-one";
 
 app.get('/', (req, res, next) => {
     res.json({
@@ -8,6 +12,26 @@ app.get('/', (req, res, next) => {
     next()
 })
 
+app.get('/api/', function (req, res, next) {
+    client.connect(connectionString,function(err,client,done) {
+       if(err){
+           console.log("not able to get connection "+ err);
+           res.status(400).send(err);
+       } 
+       client.query('SELECT * FROM Student where id = $1', [1],function(err,result) {
+           done(); // closing the connection;e
+           if(err){
+               console.log(err);
+               res.status(400).send(err);
+           }
+           res.status(200).send(result.rows);
+       });
+    });
+    next();
+});
+
 app.get('/api/v1/', (req, res) => res.send('Base v1 Req success!'))
 
 module.exports = app;
+
+ 
