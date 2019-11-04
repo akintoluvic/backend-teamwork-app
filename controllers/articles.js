ecd983f60d97a351be4e0d21bb4d3a9c91ebe677
Vick Greenfields <akintoluvic@users.noreply.github.com>
2019-11-04 16:41:03 +0100

    fix chore issue 31 folder structure

---
+++ controllers/articles.js
@@ -0,0 +1,69 @@
+const db = require('../db')
+
+const getArticles = (request, response) => {
+  db.query('SELECT * FROM articles ORDER BY articleId ASC', (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).json(results.rows)
+  })
+}
+
+// const getArticleById = (request, response) => {
+//   const id = parseInt(request.params.id)
+
+//   db.query('SELECT * FROM articles WHERE articleId = $1', [id], (error, results) => {
+//     if (error) {
+//       throw error
+//     }
+//     response.status(200).json(results.rows)
+//   })
+// }
+
+const createArticle = (request, response) => {
+  const { title, article, authorId, tag } = request.body
+
+  db.query('INSERT INTO articles (title, article, authorId, tag) VALUES ($1, $2, $3, $4)', [title, article, authorId, tag], (error, results) => {
+    if (error) {
+      throw error;
+    }
+    response.status(201).send(`Article added with ID: ${results.id}`)
+    console.log(results);
+  })
+}
+
+const updateArticle = (request, response) => {
+  const articleId = parseInt(request.params.id)
+  const {  title, article, authorId, tag } = request.body
+
+  db.query(
+    'UPDATE articles SET title = $1, article = $2, authorId = $3, tag = $4 WHERE articleId = $5',
+    [title, article, authorId, tag, articleId],
+    (error, results) => {
+      if (error) {
+        throw error
+      }
+      response.status(200).send(`Article modified with ID: ${articleId}`)
+    }
+  )
+}
+
+const deleteArticle = (request, response) => {
+  const articleId = parseInt(request.params.id)
+
+  db.query('DELETE FROM articles WHERE articleId = $1', [articleId], (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).send(`Article deleted with ID: ${articleId}`)
+  })
+}
+
+module.exports = {
+  getArticles,
+  // getArticleById,
+  createArticle,
+  updateArticle,
+  deleteArticle,
+}
+
