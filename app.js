415438f4f0b1fbf1359919875442bf22a6633c3c
Vick Greenfields <akintoluvic@users.noreply.github.com>
2019-11-04 19:18:13 +0100

    ERROR

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const bodyParser = require('body-parser');
-const { Client } = require('pg')
+const users = require('./routes/userQueries')
+const gifs = require('./routes/gifs')
+const comments = require('./routes/comments')
+const articles = require('./routes/articles')
 const app = express()
-const client = new Client()
-
-const connectionString = "postgres://postgres:greene@localhost::1/greene-one";
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -28,26 +28,30 @@ app.get('/', (req, res, next) => {
     next()
 })
 
-app.get('/api/', function (req, res, next) {
-    client.connect(connectionString,function(err,client,done) {
-       if(err){
-           console.log("not able to get connection "+ err);
-           res.status(400).send(err);
-       } 
-       client.query('SELECT * FROM Student where id = $1', [1],function(err,result) {
-           done(); // closing the connection;e
-           if(err){
-               console.log(err);
-               res.status(400).send(err);
-           }
-           res.status(200).send(result.rows);
-       });
-    });
-    next();
-});
+app.get('/users', users.getUsers)
+app.get('/users/:id', users.getUserById)
+app.post('/users', users.createUser)
+app.put('/users/:id', users.updateUser)
+app.delete('/users/:id', users.deleteUser)
+
+app.get('/gifs', gifs.getGifs)
+app.get('/gifs/:id', gifs.getGifById)
+app.post('/gifs', gifs.createGif)
+// app.put('/gifs/:id', gifs.updateGif)
+app.delete('/gifs/:id', gifs.deleteGif)
+
+app.get('/comments', comments.getComments)
+app.get('/comments/:id', comments.getCommentById)
+app.post('/comments', comments.createComment)
+// app.put('/gifs/:id', gifs.updateGif)
+app.delete('/comments/:id', comments.deleteComment)
+
+app.get('/articles', articles.getArticles)
+app.get('/articles/:id', articles.getArticleById)
+app.post('/articles', articles.createArticle)
+app.put('/articles/:id', articles.updateArticle)
+app.delete('/articles/:id', articles.deleteArticle)
 
 app.get('/api/v1/', (req, res) => res.send('Base v1 Req success!'))
 
 module.exports = app;
-
- 
