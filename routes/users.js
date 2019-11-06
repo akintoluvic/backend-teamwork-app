fbcf7dd80eb963cc95d45c97d8e7ef726c04296f
Vick Greenfields <akintoluvic@users.noreply.github.com>
2019-11-06 20:02:33 +0100

    fix

--- routes/users.js
+++ routes/users.js
@@ -64,48 +64,6 @@ const signIn = (request, response) => {
   
 }
 
-
-exports.login = (req, res, next) => {
-  User.findOne({ email: req.body.email }).then(
-    (user) => {
-      if (!user) {
-        return res.status(401).json({
-          error: new Error('User not found!')
-        });
-      }
-      bcrypt.compare(req.body.password, user.password).then(
-        (valid) => {
-          if (!valid) {
-            return res.status(401).json({
-              error: new Error('Incorrect password!')
-            });
-          }
-          const token = jwt.sign(
-            { userId: user._id },
-            'RANDOM_TOKEN_SECRET',
-            { expiresIn: '24h' });
-          res.status(200).json({
-            userId: user._id,
-            token: token
-          });
-        }
-      ).catch(
-        (error) => {
-          res.status(500).json({
-            error: error
-          });
-        }
-      );
-    }
-  ).catch(
-    (error) => {
-      res.status(500).json({
-        error: error
-      });
-    }
-  );
-}
-
 const createUser = (request, response) => {
   const { firstName, lastName, email, password, gender, jobRole, department, address } = request.body
   bcrypt.hash(password, 10).then(
