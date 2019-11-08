699fd493c0e04776133a2e0db0ef7ddb033ad938
Vick Greenfields <akintoluvic@users.noreply.github.com>
2019-11-08 20:43:51 +0100

    fix

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const userCtrl = require('../controllers/users');
 
-router.post('/auth/signin', userCtrl.signIn)
-router.post('/auth/create-user', userCtrl.createUser)
+router.post('/signin', userCtrl.signIn)
+router.post('/create-user', userCtrl.createUser)
 
 module.exports = router;
