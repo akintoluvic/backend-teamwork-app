ad94aa5d981dea68e9a34efda6d84488923acb67
Vick Greenfields <akintoluvic@users.noreply.github.com>
2019-11-09 14:03:18 +0100

    init

--- app.js
+++ app.js
@@ -7,9 +7,9 @@ const multipartMiddleware = multipart();
 
 // Routes
 const userRoutes = require('./routes/users')
-const posts = require('./routes/posts')
+const postRoutes = require('./routes/posts')
 // const gifs = require('./routes/gifs')
-const comments = require('./routes/comments')
+const commentRoutes = require('./routes/comments')
 // const articles = require('./routes/articles')
 const app = express()
 
@@ -42,39 +42,7 @@ app.get('/', (req, res, next) => {
 app.use('/auth', userRoutes)
 
 // User posts
-app.post('/gifs', multipartMiddleware, posts.createGif)
-app.post('/articles', posts.createArticle)
-app.post('/articles/:id/comments', comments.createComment) // article comments
-app.post('/gifs/:id/comments', comments.createComment) // gif comments
-
-app.delete('/articles/:id', posts.deleteArticle)
-app.delete('/gifs/:id', posts.deleteGif)
-
-
-app.get('/feed', posts.getPosts) // feed
-app.get('/articles/:id', posts.getPostById) // single article
-app.get('/gifs/:id', posts.getPostById) // single gif
-app.get('/feed/tags/:tag', posts.getPostsWithAtag) // articles with a tag
-
-// app.put('/users/:id', users.updateUser)
-// app.put('/gifs/:id', gifs.updateGif)
-app.put('/articles/:id', posts.updateArticle)
-
-
-
-// // Potential Routes
-
-// app.get('/users', users.getUsers)
-// app.get('/users/:id', users.getUserById)
-// app.delete('/users/:id', users.deleteUser)
-
-// app.get('/gifs', gifs.getGifs)
-// // app.put('/gifs/:id', gifs.updateGif)
-
-// app.get('/comments', comments.getComments)
-// app.get('/comments/:id', comments.getCommentById)
-// app.delete('/comments/:id', comments.deleteComment)
-
+app.use('/api/v1', multipartMiddleware, posts.createGif)
 
 app.get('/api/v1/', (req, res) => res.send('Base v1 Req success!'))
 
--- routes/posts.js
+++ routes/posts.js
@@ -1,217 +1,41 @@
-const db = require('../db')
-const fs = require("fs");
-const cloudinary = require('cloudinary').v2;
-const dotenv = require('dotenv');
-dotenv.config();
+const express = require('express');
+const router = express.Router();
 
-// set your env variable CLOUDINARY_URL or set the following configuration
-cloudinary.config({
-  cloud_name: process.env.CLOUD_NAME,
-  api_key: process.env.API_KEY,
-  api_secret: process.env.API_SECRET
-});
+const userCtrl = require('../controllers/users');
 
 
-// Get Routes
+app.post('/gifs', multipartMiddleware, posts.createGif)
+app.post('/articles', posts.createArticle)
+app.post('/articles/:id/comments', comments.createComment) // article comments
+app.post('/gifs/:id/comments', comments.createComment) // gif comments
 
-exports.getPosts = (request, response) => {
-    db.query('SELECT * FROM posts ORDER BY createdOn DESC', (error, results) => {
-      if (error) {
-        response.status(400).json({
-          "status": "error",
-          "error": error
-        })
-      }
-      response.status(201).json({
-        "status": "success",
-        "data": {
-          "message": "All posts returned successfully",
-          "posts": results.rows
-        }
-      })
-    })
-  }
-
-  exports.getPostById = (request, response) => {
-    const id = parseInt(request.params.id)
-  
-    db.query('SELECT * FROM posts WHERE postId = $1', [id], (error, results) => {
-    if (error) {
-        console.log(error)
-        response.status(400).json({
-          "status": "error",
-          "error": error
-        })
-      }
-      response.status(201).json({
-        "status": "success",
-        "data": {
-          "message": `Posts with the id: ${id} returned successfully`,
-          "posts": results.rows
-        }
-      })
-    })
-  }
-
-  exports.getPostsWithAtag = (request, response) => {
-    const { tag } = request.params.tag
-    db.query('SELECT * FROM posts WHERE tag = $1 ORDER BY createdOn DESC', [tag], (error, results) => {
-      if (error) {
-        console.log(error)
-        response.status(400).json({
-          "status": "error",
-          "error": error
-        })
-      }
-      response.status(201).json({
-        "status": "success",
-        "data": {
-          "message": `Posts with the tag: ${tag} returned successfully`,
-          "posts": results.rows
-        }
-      })
-    })
-  }
+app.delete('/articles/:id', posts.deleteArticle)
+app.delete('/gifs/:id', posts.deleteGif)
 
 
-  // Post Routes
+app.get('/feed', posts.getPosts) // feed
+app.get('/articles/:id', posts.getPostById) // single article
+app.get('/gifs/:id', posts.getPostById) // single gif
+app.get('/feed/tags/:tag', posts.getPostsWithAtag) // articles with a tag
 
-  exports.createArticle = (request, response) => {
-    const { title, article, authorId, tag } = request.body
-  
-    db.query('INSERT INTO posts (title, article, authorId, tag) VALUES ($1, $2, $3, $4) RETURNING postId', [title, article, authorId, tag], (error, results) => {
-      if (error) {
-        response.status(400).json({
-          "status": "error",
-          "error": error
-        })
-      }
-      response.status(201).json({
-        "status": "success",
-        "data": {
-          "message": "Article Created Successfully",
-          "articleId": results.rows[0].postid,
-          "title": title,
-          "article": article,
-          "authorId": authorId,
-          "tag": tag,
-          // "createdOn": results.rows[0].createdOn
-        }
-      })
-    })
-  }
+// app.put('/users/:id', users.updateUser)
+// app.put('/gifs/:id', gifs.updateGif)
+app.put('/articles/:id', posts.updateArticle)
 
-  exports.createGif = (request, response) => {
-    const { title, dataFile, authorId, tag } = request.body
-    let filename = request.files.dataFile.path;
-    cloudinary.uploader.upload(filename, (error, result) =>{
-      if (error) {
-        console.log(error)
-        throw error}
-    db.query('INSERT INTO posts (title, imageUrl, authorId, tag) VALUES ($1, $2, $3, $4) RETURNING postId', [title, result.url, authorId, tag], (error, results) => {
-      if (error) {
-        response.status(400).json({
-          "status": "error",
-          "error": error
-        })
-      }
-      response.status(201).json({
-        "status": "success",
-        "data": {
-          "message": "Gif created Successfully",
-          "gifId": results.rows[0].postid,
-          "title": title,
-          "imageUrl": result.url,
-          "authorId": authorId,
-          "tag": tag,
-          // "createdOn": results.rows[0].createdOn
-        }
-      })
-    })
-  })}
 
-  exports.uploadFile = (request, response) => {
-    let filename = request.files.dataFile.path;
-    cloudinary.uploader.upload(filename, (error, result) =>{
-      if (error) {
-        console.log(error)
-        throw error}
-      response.send({url: result.url, id: result.public_id})
-    }
-    )
-  }
 
-  // Delete Routes
+// // Potential Routes
 
-  exports.deleteArticle = (request, response) => {
-    const articleId = parseInt(request.params.id)
-  
-    db.query('DELETE FROM posts WHERE postId = $1', [articleId], (error, results) => {
-        if (error) {
-          response.status(400).json({
-            "status": "error",
-            "error": error
-          })
-        }
-        
-        response.status(201).json({
-          "status": "success",
-          "data": {
-            "message": "Article and comments deleted successfully",
-            "articleId": articleId
-          }
-        })
-      }
-    )
-  }
+// app.get('/users', users.getUsers)
+// app.get('/users/:id', users.getUserById)
+// app.delete('/users/:id', users.deleteUser)
 
-  exports.deleteGif = (request, response) => {
-    const gifId = parseInt(request.params.id)
-  
-    db.query('DELETE FROM posts WHERE postId = $1', [gifId], (error, results) => {
-      if (error) {
-        response.status(400).json({
-          "status": "error",
-          "error": error
-        })
-      }
-      
-      response.status(201).json({
-        "status": "success",
-        "data": {
-          "message": "Gif and comments deleted Successfully",
-          "gifId": gifId
-        }
-      })
-    })
-  }
+// app.get('/gifs', gifs.getGifs)
+// // app.put('/gifs/:id', gifs.updateGif)
 
-  // Put Routes
+// app.get('/comments', comments.getComments)
+// app.get('/comments/:id', comments.getCommentById)
+// app.delete('/comments/:id', comments.deleteComment)
 
-  exports.updateArticle = (request, response) => {
-    const articleId = parseInt(request.params.id)
-    const {  title, article, tag } = request.body
-  
-    db.query(
-      'UPDATE posts SET title = $1, article = $2, tag = $3 WHERE postId = $4',
-      [title, article, tag, articleId],
-      (error, results) => {
-        if (error) {
-          response.status(400).json({
-            "status": "error",
-            "error": error
-          })
-        }
-        response.status(201).json({
-          "status": "success",
-          "data": {
-            "message": "Article updated Successfully",
-            "articleId": results.rows[0].articleid,
-            "title": title,
-            "article": article,
-            "tag": tag
-          }
-        })
-      }
-    )
-  }
+
+module.exports = router;
