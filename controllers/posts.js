712ec0014f4dcfea8ee57deb02cbaa81b6ddf7e0
Vick Greenfields <akintoluvic@users.noreply.github.com>
2019-11-12 15:03:58 +0100

    quick refractor

--- controllers/posts.js
+++ controllers/posts.js
@@ -36,7 +36,6 @@ exports.getPosts = (request, response) => {
     const id = parseInt(request.params.id)
     db.query('SELECT * FROM posts WHERE postId = $1', [id], (error, results) => {
     if (error) {
-        console.log(error)
         response.status(400).json({
           "status": "error",
           "error": error
