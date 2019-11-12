1ecc878a960c51a0b5ef983bfdcb0b04f1712a93
Vick Greenfields <akintoluvic@users.noreply.github.com>
2019-11-12 19:15:08 +0100

    fix post routes

--- routes/posts.js
+++ routes/posts.js
@@ -1,33 +1,31 @@
 const express = require('express');
+
 const router = express.Router();
-const multipart = require("connect-multiparty");                        
+const multipart = require('connect-multiparty');
+
 const multipartMiddleware = multipart();
 
-const auth = require('../middleware/auth')
+const auth = require('../middleware/auth');
 const posts = require('../controllers/posts');
 const comments = require('../controllers/comments');
 
+router.post('/gifs', auth, multipartMiddleware, posts.createGif);
+router.post('/articles', auth, posts.createArticle);
+router.post('/articles/:id/comment', auth, comments.createComment); // article comments
+router.post('/gifs/:id/comment', auth, comments.createComment); // gif comments
 
-router.post('/gifs', auth, multipartMiddleware, posts.createGif)
-router.post('/articles', auth, posts.createArticle)
-router.post('/articles/:id/comment', auth, comments.createComment) // article comments
-router.post('/gifs/:id/comment', auth, comments.createComment) // gif comments
+router.delete('/articles/:id', auth, posts.deleteArticle);
+router.delete('/gifs/:id', auth, posts.deleteGif);
 
-router.delete('/articles/:id', auth, posts.deleteArticle)
-router.delete('/gifs/:id', auth, posts.deleteGif)
-
-
-router.get('/feed', auth, posts.getPosts) // feed
-router.get('/articles/:id', auth, posts.getPostById) // single article
-router.get('/gifs/:id', auth, posts.getPostById) // single gif
-router.get('/feed/tags/:tag', auth, posts.getPostsWithAtag) // articles with a tag
+router.get('/feed', auth, posts.getPosts); // feed
+router.get('/articles/:id', auth, posts.getPostById); // single article
+router.get('/gifs/:id', auth, posts.getPostById); // single gif
+router.get('/feed/tags/:tag', auth, posts.getPostsWithAtag); // articles with a tag
 
 // router.put('/users/:id', users.updateUser)
 // router.put('/gifs/:id', gifs.updateGif)
-router.put('/articles/:id', auth, posts.updateArticle)
-router.put('/gifs/:id', auth, posts.updateGif)
-
-
+router.put('/articles/:id', auth, posts.updateArticle);
+router.put('/gifs/:id', auth, posts.updateGif);
 
 // // Potential Routes
 
@@ -41,6 +39,5 @@ router.put('/gifs/:id', auth, posts.updateGif)
 // router.get('/comments', comments.getComments)
 // router.get('/comments/:id', comments.getCommentById)
 // router.delete('/comments/:id', comments.deleteComment)
-
 
 module.exports = router;
