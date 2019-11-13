5008a5e9a9029a84d8af69c5848b4918ad7e5f8e
Vick Greenfields <akintoluvic@users.noreply.github.com>
2019-11-13 16:11:49 +0100

    fix #67 feature get profile route

--- routes/users.js
+++ routes/users.js
@@ -8,5 +8,6 @@ const auth = require('../middleware/auth');
 router.post('/signin', userCtrl.signIn);
 router.post('/create-user', auth, userCtrl.createUser);
 router.put('/update-profile', auth, userCtrl.updateUser);
+router.get('/profile/:id', auth, userCtrl.getProfile);
 
 module.exports = router;
