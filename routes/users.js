31c0d661c99c4b678593371002d3ec3a1841eb50
Vick Greenfields <akintoluvic@users.noreply.github.com>
2019-11-14 20:43:21 +0100

    fix create admin

--- routes/users.js
+++ routes/users.js
@@ -6,7 +6,7 @@ const userCtrl = require('../controllers/users');
 const auth = require('../middleware/auth');
 
 router.post('/signin', userCtrl.signIn);
-router.post('/create-user', auth, userCtrl.createUser);
+router.post('/create-user', userCtrl.createUser);
 router.put('/update-profile', auth, userCtrl.updateUser);
 router.get('/profile/:id', auth, userCtrl.getProfile);
 
