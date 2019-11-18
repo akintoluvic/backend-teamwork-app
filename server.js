13379abbe4d3f3de1fb70aeb4fd6d3e5bca2c799
Vick Greenfields <akintoluvic@users.noreply.github.com>
2019-11-18 16:35:16 +0100

    still trying

--- server.js
+++ server.js
@@ -3,15 +3,11 @@ const dotenv = require('dotenv');
 const app = require('./app');
 
 dotenv.config();
-// eslint-disable-next-line no-unused-vars
-// eslint-disable-next-line no-multi-assign
-const exports = (module.exports = {});
+var exports = module.exports = {};
 
 const port = process.env.PORT || 3000;
 app.set('port', port);
 
-const server = http.createServer(app);
+const server = http.createServer(app)
 
-if (!module.parent) {
-  server.listen(port);
-}
+if(!module.parent){ server.listen(port); }
