fb9611a01039e81f510392bab0bfc7e6b8551794
Vick Greenfields <akintoluvic@users.noreply.github.com>
2019-11-18 14:34:23 +0100

    still fixing

--- spec/routesSpec.js
+++ spec/routesSpec.js
@@ -5,143 +5,171 @@ const userTest = require('../app.js');
 
 const baseUrl = 'http://localhost:3000/api/v1';
 
+beforeEach(function(done) {
+  utils.simulateAsyncOp(function() {
+    asyncOpCompleted = true;
+    done();
+  });
+});
+
 describe('User Test Server', function() {
-  describe('POST /', function() {
-    it('returns status code 200', function(done) {
-      request.post(
-        `${baseUrl}/auth/signin`,
-        {
-          json: true,
-          body: {
-            email: 'zaeddade@rty.com',
-            password: 'me367mjnfdw'
-          }
-        },
-        function(error, response) {
-          expect(response.statusCode).toBe(200);
-          expect(response.body.status).toBe('success');
-          expect(response.body.status).toBe('success');
-          done();
+  let token;
+  it('returns status code 200', function(done) {
+    request.post(
+      `${baseUrl}/auth/signin`,
+      {
+        json: true,
+        body: {
+          email: 'zaeddade@rty.com',
+          password: 'me367mjnfdw'
         }
-      );
-    });
-  });
-  describe('POST /', function() {
-    it('returns status code 200', function(done) {
-      request.post(
-        `${baseUrl}/auth/create-user`,
-        {
-          json: true,
-          body: {
-            email: 'fatddade@rty.com',
-            password: 'me2367mjnfdw'
+      },
+      function(error, response) {
+        const { token } = response.body;
+        expect(response.statusCode).toBe(200);
+        expect(response.body.status).toBe('success');
+        expect(response.body.status).toBe('success');
+        done();
+      }
+    );
+    describe('POST /', function() {
+      it('returns status code 200', function(done) {
+        request.post(
+          `${baseUrl}/auth/create-user`,
+          {
+            json: true,
+            body: {
+              email: 'fatddade@rty.com',
+              password: 'me2367mjnfdw'
+            }
+          },
+          { Authorization: token },
+          function(error, response) {
+            expect(response.statusCode).toBe(201);
+            expect(response.body.status).toBe('success');
+            done();
           }
-        },
-        function(error, response) {
+        );
+      });
+    });
+    describe('POST /', function() {
+      it('returns status code 200', function(done) {
+        request.post(
+          `${baseUrl}/auth/create-user`,
+          {
+            json: true,
+            body: {
+              email: 'fatddade@rty.com',
+              password: 'me2367mjnfdw'
+            }
+          },
+          { Authorization: token },
+          function(error, response) {
+            expect(response.statusCode).toBe(401);
+            expect(response.body.status).toBe('error');
+            done();
+          }
+        );
+      });
+    });
+    describe('GET /', function() {
+      it('returns status code 201', function(done) {
+        request.get(`${baseUrl}/feed`, function(error, response) {
           expect(response.statusCode).toBe(401);
-          expect(response.body.status).toBe('error');
+          expect(response.body.status).toBe(undefined);
           done();
-        }
-      );
-    });
-  });
-  describe('GET /', function() {
-    it('returns status code 201', function(done) {
-      request.get(`${baseUrl}/feed`, function(error, response) {
-        expect(response.statusCode).toBe(401);
-        expect(response.body.status).toBe(undefined);
-        done();
+        });
       });
     });
-  });
-  describe('GET /', function() {
-    it('returns status code 201', function(done) {
-      request.get(`${baseUrl}/gifs/3`, function(error, response) {
-        expect(response.statusCode).toBe(401);
-        done();
-      });
-    });
-  });
-  describe('GET /', function() {
-    it('returns status code 201', function(done) {
-      request.get(`${baseUrl}/feed/tags/work`, function(error, response) {
-        expect(response.statusCode).toBe(401);
-        done();
-      });
-    });
-  });
-  describe('PUT /', function() {
-    it('returns status code 201', function(done) {
-      request.put(
-        `${baseUrl}/gifs/1`,
-        {
-          json: true,
-          body: {
-            title: 'zaeddade@rty.com',
-            imageUrl: 'me367mjnfdw',
-            tag: 'work'
-          }
-        },
-        function(error, response) {
+    describe('GET /', function() {
+      it('returns status code 201', function(done) {
+        request.get(`${baseUrl}/gifs/3`, function(error, response) {
           expect(response.statusCode).toBe(401);
           done();
-        }
-      );
-    });
-  });
-  describe('PUT /', function() {
-    it('returns status code 201', function(done) {
-      request.put(
-        `${baseUrl}/articles/1`,
-        {
-          json: true,
-          body: {
-            title: 'zaeddade@rty.com',
-            article: 'me367mjnfdw',
-            tag: 'work'
-          }
-        },
-        function(error, response) {
-          expect(response.statusCode).toBe(401);
-          done();
-        }
-      );
-    });
-  });
-  describe('DELETE /', function() {
-    it('returns status code 201', function(done) {
-      request.delete(`${baseUrl}/articles/1`, function(error, response) {
-        expect(response.statusCode).toBe(401);
-        done();
+        });
       });
     });
-  });
-  describe('DELETE /', function() {
-    it('returns status code 201', function(done) {
-      request.delete(`${baseUrl}/gifs/1`, function(error, response) {
-        expect(response.statusCode).toBe(401);
-        done();
-      });
-    });
-  });
-  describe('POST /', function() {
-    it('returns status code 201', function(done) {
-      request.post(
-        `${baseUrl}/articles`,
-        {
-          json: true,
-          body: {
-            title: 'fatddade@rty.com',
-            article: 'me2367mjnfdw',
-            authorId: 7,
-            tag: 'work'
-          }
-        },
-        function(error, response) {
+    describe('GET /', function() {
+      it('returns status code 201', function(done) {
+        request.get(`${baseUrl}/feed/tags/work`, function(error, response) {
           expect(response.statusCode).toBe(401);
           done();
-        }
-      );
+        });
+      });
+    });
+    describe('PUT /', function() {
+      it('returns status code 201', function(done) {
+        request.put(
+          `${baseUrl}/gifs/1`,
+          {
+            json: true,
+            body: {
+              title: 'zaeddade@rty.com',
+              imageUrl: 'me367mjnfdw',
+              tag: 'work'
+            }
+          },
+          function(error, response) {
+            expect(response.statusCode).toBe(401);
+            done();
+          }
+        );
+      });
+    });
+    describe('PUT /', function() {
+      it('returns status code 201', function(done) {
+        request.put(
+          `${baseUrl}/articles/1`,
+          {
+            json: true,
+            body: {
+              title: 'zaeddade@rty.com',
+              article: 'me367mjnfdw',
+              tag: 'work'
+            }
+          },
+          function(error, response) {
+            expect(response.statusCode).toBe(401);
+            done();
+          }
+        );
+      });
+    });
+    describe('DELETE /', function() {
+      it('returns status code 201', function(done) {
+        request.delete(`${baseUrl}/articles/1`, function(error, response) {
+          expect(response.statusCode).toBe(401);
+          done();
+        });
+      });
+    });
+    describe('DELETE /', function() {
+      it('returns status code 201', function(done) {
+        request.delete(`${baseUrl}/gifs/1`, function(error, response) {
+          expect(response.statusCode).toBe(401);
+          done();
+        });
+      });
+    });
+    describe('POST /', function() {
+      it('returns status code 201', function(done) {
+        request.post(
+          `${baseUrl}/articles`,
+          {
+            json: true,
+            body: {
+              title: 'fatddade@rty.com',
+              article: 'me2367mjnfdw',
+              authorId: 7,
+              tag: 'work'
+            }
+          },
+          function(error, response) {
+            expect(response.statusCode).toBe(401);
+            done();
+          }
+        );
+      });
     });
   });
 });
