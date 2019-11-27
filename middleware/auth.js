47bd863caf7ff3d7ff382c8d213ebe9f500661fb
Vick Greenfields <akintoluvic@users.noreply.github.com>
2019-11-27 17:46:57 +0100

    init

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,9 +3,11 @@ const jwt = require('jsonwebtoken');
 module.exports = (req, res, next) => {
   try {
     const token = req.headers.authorization.split(' ')[1];
+    const id = req.headers.authorization.split(' ')[2];
     const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
-    const { userId } = decodedToken;
-    if (req.body.userId && req.body.userId !== userId) {
+    // const { userId } = decodedToken;
+    // if (req.body.userId && req.body.userId !== userId) {
+    if (id !== decodedToken) {
       res.status(401).json({
         message: 'Invalid user ID',
         error: 'error'
@@ -16,7 +18,8 @@ module.exports = (req, res, next) => {
   } catch (error) {
     res.status(401).json({
       error: 'You are not logged in, please Login first',
-      status: 'error'
+      status: 'error',
+      req: req.headers.authorization.split(' ')[1]
     });
   }
 };
